a5a6c57b507085f9afc892dbd1932a05a8e0e4b5
Choiwonwong <chldnjsdnd12@naver.com>
2023-10-15 14:07:05 +0900

    SSE 초안 Implement

--- src/components/sse-test.js
+++ src/components/sse-test.js
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [logData, setLogData] = useState([]); // 배열로 받아서 여러 내용을 출력
+  const [logData, setLogData] = useState([]);
 
   useEffect(() => {
-    const eventSource = new EventSource("http://localhost:8000/api/logs");
+    const eventSource = new EventSource(
+      "http://localhost:8080/api/requests/1/provision/logs"
+    );
 
     eventSource.onmessage = (event) => {
-      setLogData((prevLogData) => [...prevLogData, event.data]); // 이전 데이터와 현재 데이터를 합침
+      setLogData((prevLogData) => [...prevLogData, event.data]);
     };
 
     eventSource.onerror = (error) => {
@@ -15,10 +17,21 @@ function App() {
       eventSource.close();
     };
 
+    const pingInterval = setInterval(() => {
+      eventSource.dispatchEvent(new Event("ping"));
+    }, 5000); // Send a "ping" message every 5 seconds
+
+    // Add an event listener to send a disconnect message when the page is about to unload
+    window.addEventListener("beforeunload", () => {
+      eventSource.close(); // Close the SSE connection
+      // You can also send a disconnect message to the server here if needed
+    });
+
     return () => {
+      clearInterval(pingInterval);
       eventSource.close();
     };
-  }, []);
+  }, []); // Empty dependency array to ensure it runs only once
 
   return (
     <div className="App">
@@ -33,3 +46,41 @@ function App() {
 }
 
 export default App;
+
+// import React, { useEffect, useState } from "react";
+
+// function App() {
+//   const [logData, setLogData] = useState([]);
+
+//   useEffect(() => {
+//     const eventSource = new EventSource(
+//       "http://localhost:8080/api/requests/1/provision/logs"
+//     );
+
+//     eventSource.onmessage = (event) => {
+//       setLogData((prevLogData) => [...prevLogData, event.data]);
+//     };
+
+//     eventSource.onerror = (error) => {
+//       console.error("SSE Error:", error);
+//       eventSource.close();
+//     };
+
+//     return () => {
+//       eventSource.close();
+//     };
+//   }, []);
+
+//   return (
+//     <div className="App">
+//       <h1>Received Messages:</h1>
+//       <ul>
+//         {logData.map((log, index) => (
+//           <li key={index}>{log}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default App;
